8a49e47774c1441e96b928410f35c580ca5b4e2f
ebash0 <ebash.0@yandex.com>
2017-01-17 12:34:03 +0200

    upload

---
+++ config/db_params.php
@@ -0,0 +1,11 @@
+<?php
+
+
+return array (
+
+        // Массив с параметрами подключения к базе данных
+        'server' => 'localhost',
+        'database' => 'shop',
+        'username' => 'shop',
+        'password' => 'shop',
+);
